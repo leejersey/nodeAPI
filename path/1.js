0a32de7406b5e887ce336cb4529a8c557976cdad
jersey <lizexi711@gmail.com>
2015-09-30 10:18:06 +0800

    path change

--- path/1.js
+++ path/1.js
@@ -7,3 +7,15 @@ console.log(path.join('/foo', 'bar', 'baz/asdf', 'quux', '..'));
 console.log(path.resolve('foo/bar', '/tmp/file/', '..', 'a/../subfile'));
 
 console.log(path.relative('/data/orandea/test/aaa', '/data/orandea/impl/bbb'));
+
+console.log(path.dirname('/foo/bar/baz/asdf/quux'));
+
+console.log(path.basename('/foo/bar/baz/asdf/quux.html'));
+
+console.log(path.extname('index.html'));
+
+console.log(path.sep);
+console.log('foo/bar/baz'.split(path.sep));
+
+
+console.log(path.delimiter);
